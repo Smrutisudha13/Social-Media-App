77436ca21d5c8db2256c2e6ba48966878ba8f110
Smrutisudha <senapatismrutisudha@gmail.com>
2024-03-16 17:55:50 +0530

    Connecting to mongo

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+
 const app = express();
 
-app.use("/api",(req,res,next) =>{
-    res.send("Hello World");
-})
 
-app.listen(5000);
+
+mongoose
+  .connect(
+    "mongodb+srv://senapatismrutisudha:K1VnBKetVRxSWC9r@socialmediaapp.synh69f.mongodb.net/SocialMediaApp?retryWrites=true&w=majority&appName=SocialMediaApp"
+  )
+  .then(() => app.listen(5000))
+  .then(() => console.log("Connected to DB and Listening to localhost 5000"))
+  .catch((err) => console.log(err));
